e1a8a30190860e625beb1d032020a4a0949a95d4
vincitamore <vincit_amore@amore.build>
2025-01-19 20:57:39 -0600

    add X integration v26

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,25 @@ const nextConfig = {
   },
   experimental: {
     optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react'],
+    turbotrace: {
+      logLevel: 'error',
+      contextDirectory: '.',
+      processCwd: '.',
+    },
+    outputFileTracingRoot: '.',
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@esbuild/linux-x64',
+        '.git/**/*',
+        '**/*.{pem,pdf,txt,log,md}',
+        '**/cache/**',
+      ],
+    },
+  },
+  typescript: {
+    ignoreBuildErrors: false,
   },
 }
 
