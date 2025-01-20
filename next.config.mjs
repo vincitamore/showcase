c3d360350f7215a5302cdc3156672cd8fc35368c
vincitamore <vincit_amore@amore.build>
2025-01-19 20:54:20 -0600

    add X integration v25

--- next.config.mjs
+++ next.config.mjs
@@ -12,17 +12,6 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react'],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        net: false,
-        tls: false,
-        fs: false,
-      };
-    }
-    return config;
-  },
 }
 
 export default nextConfig 
