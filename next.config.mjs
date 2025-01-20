8dfe1bcc26a6d02443b3049e2012826155cd2ecc
vincitamore <vincit_amore@amore.build>
2025-01-19 20:04:41 -0600

    add X integration v14

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,27 @@ const nextConfig = {
   },
   experimental: {
     optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react'],
+    serverComponentsExternalPackages: ['twitter-api-v2'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        https: false,
+        http: false,
+        zlib: false,
+        path: false,
+        stream: false,
+        util: false,
+        url: false,
+        os: false,
+      };
+    }
+    return config;
   },
 }
 
