be555212d9210513e1f2b51e82f7ca4e11029ea7
vincitamore <vincit_amore@amore.build>
2025-01-19 20:14:51 -0600

    add X integration v15

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
